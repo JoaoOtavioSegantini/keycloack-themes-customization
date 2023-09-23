d48cef47162eea154a7624277c8fdabdeb601c1c
João Otávio <joaotavioos@outlook.com>
2023-09-23 18:19:14 -0300

    feat: add copy to common paths ✨

---
+++ scripts/_make-commom-resources.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+const fsExtra = require("fs-extra");
+const paths = require("../config/paths");
+
+module.exports = () => {
+    const themesDir = fs.readdirSync(paths.appBuild);
+    for (const themeDir of themesDir) {
+      const jsDir = path.join(paths.appBuild, themeDir, "js");
+      if (fs.existsSync(jsDir)) {
+        fsExtra.copySync(
+          path.join(jsDir),
+          path.join(paths.appBuild, themeDir, "common", "resources", "js")
+        );
+      }
+  
+      const cssDir = path.join(paths.appBuild, themeDir, "css");
+      if (fs.existsSync(cssDir)) {
+        fsExtra.copySync(
+          path.join(cssDir),
+          path.join(paths.appBuild, themeDir, "common", "resources", "css")
+        );
+      }
+    }
+  
+}
