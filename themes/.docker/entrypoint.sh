fd6ea1c1cc342f28e13a6325dcebb4572eac1cab
João Otávio <joaotavioos@outlook.com>
2023-09-25 21:24:48 -0300

    fix: add fork ts checker dependencies 🐛

--- themes/.docker/entrypoint.sh
+++ themes/.docker/entrypoint.sh
@@ -2,8 +2,9 @@
 
 npm install
 
-npm install --prefix ./config/fork-ts-checker-webpack-plugin
+#npm install --prefix ./config/fork-ts-checker-webpack-plugin
+cp -R ./node_modules/fork-ts-checker-webpack-plugin/node_modules ./config/fork-ts-checker-webpack-plugin
 
-npm build
+npm run build
 
 /bin/sh -c "while sleep 1000; do :; done"
