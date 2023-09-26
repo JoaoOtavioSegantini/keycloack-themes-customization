8dca2acc6a2267562dd5dbf86ffab11ec0fea8ba
João Otávio <joaotavioos@outlook.com>
2023-09-26 05:10:07 -0300

    fix: If build folder exist, does not create it 🐛

--- themes/.docker/entrypoint.sh
+++ themes/.docker/entrypoint.sh
@@ -5,6 +5,11 @@ npm install
 #npm install --prefix ./config/fork-ts-checker-webpack-plugin
 cp -R ./node_modules/fork-ts-checker-webpack-plugin/node_modules ./config/fork-ts-checker-webpack-plugin
 
-npm run build
+if [ -d ./build ];
+then
+  echo "A pasta build já existe, caso queria refazê-la, é preciso executar o comando npm run build manualmente"
+else
+  npm run build
+fi
 
 /bin/sh -c "while sleep 1000; do :; done"
